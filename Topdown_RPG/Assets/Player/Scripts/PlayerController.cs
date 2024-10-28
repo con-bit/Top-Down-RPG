c2ad34eb6d3b4cb599cfc4cdeb5dcf9cae9703d3
Element7213 <154471123+Element7213@users.noreply.github.com>
2024-10-28 14:41:21 -0700

    Roll progress
    Added 2 methods that are used to roll, StartRoll and EndRoll. Added new variables to roll.

--- Topdown_RPG/Assets/Player/Scripts/PlayerController.cs
+++ Topdown_RPG/Assets/Player/Scripts/PlayerController.cs
@@ -15,6 +15,13 @@ public class PlayerController : MonoBehaviour
     // speed to be changed in our code
     private float speed;
 
+    [SerializeField] private float rollSpeed = 10f;        // Speed of the roll
+    [SerializeField] private float rollDuration = 0.2f;    // How long the roll lasts
+    [SerializeField] private float rollCooldown = 1f;      // Cooldown time before the player can roll again
+    private bool isRolling = true;
+    private float rollEndTime;
+    private float lastRollTime;
+
     // reference to game object's rigidbody behavior
     private Rigidbody2D rb;
 
@@ -30,9 +37,12 @@ public class PlayerController : MonoBehaviour
     {
         // note: using GetAxis() allows use to have some smoothing and damping 
         // built into the input manager, can change to GetAxisRaw() for snappier movement
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical")).normalized * speed;
 
+        //// sprinting
         // if shift is pressed, speed increases to sprint value. otherwise, speed stays its original value.
-        if (Input.GetKey("left shift"))
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             speed = sprint;
         }
@@ -41,8 +51,35 @@ public class PlayerController : MonoBehaviour
             speed = walkingSpeed;
         }
 
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal"),
-            Input.GetAxis("Vertical")).normalized * speed;
+        //// rolling
+        // Check if the player presses the roll key and roll is off cooldown and player is moving
+        if (Input.GetKeyDown(KeyCode.Space) && (Time.time > lastRollTime + rollCooldown) && !isRolling && ((Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)))
+        {
+            Debug.Log("Rolling");
+            StartRoll();
+        }
+        // If rolling, check if the roll duration has ended
+        if (isRolling && Time.time > rollEndTime)
+        {
+            Debug.Log("End Rolling");
+            EndRoll();
+        }
 
+    }
+
+    private void StartRoll()
+    {
+        isRolling = true;
+        lastRollTime = Time.time; // grabs time of roll based on game time
+        rollEndTime = Time.time + rollDuration;
+
+        // Set roll velocity in the direction of player movement
+        Vector2 rollDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        rb.velocity = rollDirection * rollSpeed;
+    }
+    private void EndRoll()
+    {
+        isRolling = false;
+        rb.velocity = Vector2.zero;  // Stop roll by resetting velocity
     }
 }
